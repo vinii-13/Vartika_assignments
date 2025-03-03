7b6abf58b838229ef52d4832189dbf79f9d97e60
Vartika Sharma <96379331+vinii-13@users.noreply.github.com>
2025-03-03 14:42:47 +0530

    Add files via upload

---
+++ shallow_deep_copy_with_intPtr.cpp
@@ -0,0 +1,29 @@
+//#include<iostream>
+//using namespace std;
+//class ab {
+//	int a;
+//	int* ptr;
+//public:// ERRONOUS CODE
+//	ab() {
+//		a = 0;
+//		ptr = new int(100);
+//	}
+//	 
+//
+//	// deep copy
+//	ab(const ab &c) {
+//		a = c.a;
+//		ptr = new int(*c.ptr);
+//	}
+//
+//	~ab() {
+//		delete ptr;
+//	}
+//};
+//
+//int main() {
+//	ab z;
+//	ab z1(z);
+//
+//	return 0;
+//}
