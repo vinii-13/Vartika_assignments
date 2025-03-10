2836b59ac1d9c09705124fb96fa178b25fef93f6
Vartika Sharma <96379331+vinii-13@users.noreply.github.com>
2025-03-10 14:09:22 +0530

    Add files via upload

---
+++ namespaces/namespace.h
@@ -0,0 +1,26 @@
+//#include<iostream>
+//using namespace std;
+//
+//namespace fruits {
+//	string name = "Papaya";
+//	string type = "cold fruit";
+//
+//	void description() {
+//		cout << "Papaya is a cold fruit" << endl;
+//	}
+//
+//	class fruit {
+//		string arr[5];
+//	public:
+//		fruit() {
+//			arr[0] = "apple";
+//			arr[1] = "banana";
+//			arr[2] = "pear";
+//		}
+//		void display() {
+//			for (int i = 0; i < 5; i++) {
+//				cout << arr[i] << endl;
+//			}
+//		}
+//	};
+//}
